2af8a5fbb7d4c73ec0cae1f82459bb2cf8412de4
vandan maheshwari <vandanmaheshwari9407@gmail.com>
2023-04-25 08:10:23 +0530

    Pictures uploaded

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ def predict_datapoint():
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=True,port=5001)
-    # http://127.0.0.1:5000/
+    app.run(host='0.0.0.0',port=8080)
+    # app.run(host='0.0.0.0',debug=True,port=5001)
+    # http://127.0.0.1:8080/
   
